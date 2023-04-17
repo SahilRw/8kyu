28e99d83dd21e422243b0d797cde267899f6d4cd
Sahil R <sahilrw000@gmail.com>
2023-04-17 12:44:27 +0530

    return count of positive and sum of negatives in an array

---
+++ positiveandnegative.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// my solution
+function countPositivesSumNegatives(input) {
+  return (input && input.length) ? [
+    input.filter(x => x > 0).length,
+    input.reduce((a, b) => b < 0 ? a + b : a, 0)
+  ] : []
+}
