365811fd57b9c8cf6059ac12bb7dbe6e3f3f3794
Sahil R <sahilrw000@gmail.com>
2023-04-17 12:25:59 +0530

    squaring an argument

---
+++ square.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// my solution
+// Write the "square"-function here
+function square(num){
+  return num * num
+}
