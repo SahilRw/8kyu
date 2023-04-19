d464cf6e9cc614987a8ff01171aed275bf10519a
Sahil R <sahilrw000@gmail.com>
2023-04-19 09:56:02 +0530

    remove string spaces

---
+++ rem-str-space.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// my solution
+function noSpace(x){
+  return x.split(' ').join('')
+}
