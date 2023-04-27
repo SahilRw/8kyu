1800f6b60c44329fbf701a9944aa051ff1e4e5ed
Sahil R <sahilrw000@gmail.com>
2023-04-27 09:29:20 +0530

    return to sanity

---
+++ sanity.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// my solution
+function mystery() {
+  var results ={
+    sanity: 'Hello'
+  };
+  return results
+}
